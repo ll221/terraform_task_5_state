b23a55e93dac58337bbe8d506af612e2c8209b20
ll221 <ihor.kunakh.kb.2022@lpnu.ua>
2026-08-18 13:39:24 +0300

    1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.8.4"
-  
+
   backend "azurerm" {
     resource_group_name  = "tfstate"
     storage_account_name = "tfstateaccount2024"
@@ -8,7 +8,7 @@ terraform {
     key                  = "terraform.tfstate"
     use_oidc             = true
   }
-  
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
